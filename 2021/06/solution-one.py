47c83bcd9edfb73d09acc45c59516fed8467e816
kehrman@bignerdranch.com <kehrman@bignerdranch.com>
2021-12-10 17:29:00 -0500

    Solution for problem 01 minus reading data from file. May skip that.

--- 2021/06/solution-one.py
+++ 2021/06/solution-one.py
@@ -1,4 +1,22 @@
 # Advent of Code 2021
 # Day 06
 # Problem 01
+initial_state = [3, 4, 3, 1, 2]
+population = [0]*9
 
+for day in initial_state:
+    population[day] += 1
+
+noobs = 0
+for day in range(0, 80):
+    print(population)
+
+    next = [0]*9
+    noobs = population[0]
+    for age in range(0, 8):
+        next[age] = population[age+1]
+    next[6] += noobs
+    next[8] = noobs
+    population = next
+
+print(sum(population))
